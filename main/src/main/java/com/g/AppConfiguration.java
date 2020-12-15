200a6c468a237d6286ee758bda5ec13b9ceddde9
Gaven <gaven_yeung@163.com>
2020-12-15 11:52:03 +0800

    ADD: app configuration

---
+++ main/src/main/java/com/g/AppConfiguration.java
@@ -0,0 +1,24 @@
+package com.g;
+
+import java.text.SimpleDateFormat;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import com.g.commons.utils.DateTimePattern;
+
+@Configuration
+public class AppConfiguration {
+    @Bean
+    public ObjectMapper objectMapper() {
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+        mapper.setDateFormat(new SimpleDateFormat(DateTimePattern.DF_YYYY_MM_DD));
+        mapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
+        return mapper;
+    }
+}
