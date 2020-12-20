ba8dce19068e4cc9f0461c5e1bfb5152f51a61b6
Gaven <gaven_yeung@163.com>
2020-12-20 15:22:14 +0800

    UPDATE: switch to import xml resource file

--- main/src/main/java/com/g/config/AppConfig.java
+++ main/src/main/java/com/g/config/AppConfig.java
@@ -6,6 +6,7 @@ import java.util.Properties;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ImportResource;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
 
@@ -17,6 +18,7 @@ import com.g.commons.utils.DateTimePattern;
 import com.g.commons.utils.MailSender;
 
 @Configuration
+@ImportResource("classpath*:mail-config.xml")
 public class AppConfig {
     @Bean
     public ObjectMapper objectMapper() {
@@ -25,30 +27,5 @@ public class AppConfig {
         mapper.setDateFormat(new SimpleDateFormat(DateTimePattern.DF_YYYY_MM_DD));
         mapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
         return mapper;
-    }
-
-    @Bean
-    @Qualifier("gsgdgcyxgs_pm@163.com")
-    public JavaMailSender javaMailSender() {
-        Properties properties = new Properties();
-        properties.put("mail.smtp.host", "smtp.163.com");
-        properties.put("mail.smtp.port", 465);
-        properties.put("mail.smtp.auth", true);
-        properties.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
-        properties.put("mail.smtp.socketFactory.port", 465);
-        properties.put("mail.debug", false);
-        properties.put("mail.smtp.starttls.enable", true);
-
-        JavaMailSenderImpl javaMailSender = new JavaMailSenderImpl();
-        javaMailSender.setJavaMailProperties(properties);
-        javaMailSender.setUsername("gsgdgcyxgs_pm@163.com");
-        javaMailSender.setPassword("abcde12345");
-
-        return javaMailSender;
-    }
-
-    @Bean
-    public MailSender mailSender(@Qualifier("gsgdgcyxgs_pm@163.com") JavaMailSender javaMailSender) {
-        return new MailSender("gsgdgcyxgs_pm@163.com", javaMailSender);
     }
 }
