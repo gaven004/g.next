6698e15c98916bdf48b60d59f100fe050a33487f
Gaven <gaven_yeung@163.com>
2021-01-12 10:47:15 +0800

    UPDATE:

--- main/src/test/java/com/g/sys/prop/SysPropertyCategoriesWebTest.java
+++ main/src/test/java/com/g/sys/prop/SysPropertyCategoriesWebTest.java
@@ -1,5 +1,6 @@
 package com.g.sys.prop;
 
+import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.CoreMatchers.notNullValue;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
@@ -16,11 +17,22 @@ import com.g.WebApplicationTest;
 public class SysPropertyCategoriesWebTest extends WebApplicationTest {
     @Test
     void find() throws Exception {
-        mockMvc.perform(get("/sys/property/categories?status=VALID&status=INVALID&page=0&size=10")
+        mockMvc.perform(get("/sys/property/categories?status=VALID&status=INVALID&current=1&pageSize=20&sorter=%7B%22id%22%3A%22ascend%22%7D&filter=%7B%7D")
                 .contentType(MediaType.APPLICATION_JSON))
                 .andDo(print())
                 .andExpect(status().isOk())
-                .andExpect(jsonPath("$.content", notNullValue()));
+                .andExpect(jsonPath("$.success", is(true)))
+                .andExpect(jsonPath("$.data", notNullValue()));
+    }
+
+    @Test
+    void getOptions() throws Exception {
+        mockMvc.perform(get("/sys/property/categories/$options")
+                .contentType(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.success", is(true)))
+                .andExpect(jsonPath("$.data", notNullValue()));
     }
 
     @Test
