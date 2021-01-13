58e01103e1889d3cc62b027da243b488b585f405
Gaven <gaven_yeung@163.com>
2021-01-13 12:02:22 +0800

    ADD: Test config

--- main/src/test/java/com/g/NextApplicationTests.java
+++ main/src/test/java/com/g/NextApplicationTests.java
@@ -1,14 +1,20 @@
 package com.g;
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootTest
 public class NextApplicationTests {
+    @Autowired
+    ApplicationContext context;
 
     @Test
     void contextLoads() {
+        assertNotNull(context);
     }
-
 }
