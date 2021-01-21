92275ff5a3df6c4f0f3020680cb93a3564188e45
Gaven <gaven_yeung@163.com>
2021-01-21 17:19:02 +0800

    UPDATE: 使用统一配置的ObjectMapper

--- main/src/main/java/com/g/config/AppConfig.java
+++ main/src/main/java/com/g/config/AppConfig.java
@@ -2,6 +2,7 @@ package com.g.config;
 
 import org.springframework.boot.autoconfigure.AutoConfigureBefore;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.web.servlet.WebMvcAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.DependsOn;
@@ -13,7 +14,8 @@ import com.g.commons.utils.ObjectMapperUtil;
 
 @Configuration
 // 强制先于DataSourceAutoConfiguration执行，以先创建ApplicationContextHolder
-@AutoConfigureBefore(DataSourceAutoConfiguration.class)
+// 强制先于WebMvcAutoConfiguration执行，以先创建ObjectMapper
+@AutoConfigureBefore({DataSourceAutoConfiguration.class, WebMvcAutoConfiguration.class})
 @ImportResource("classpath*:mail-config.xml")
 public class AppConfig {
     @Bean
@@ -21,6 +23,7 @@ public class AppConfig {
     // 并不是ObjectMapper本身依赖
     @DependsOn("applicationContextHolder")
     public ObjectMapper objectMapper() {
+        // 采用统一配置的ObjectMapper
         return ObjectMapperUtil.getObjectMapper();
     }
 }
