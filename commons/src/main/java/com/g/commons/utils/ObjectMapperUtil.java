5954bb0b01caebb684827183cb92b63a01fc255f
Gaven <gaven_yeung@163.com>
2021-01-21 12:24:45 +0800

    ADD: mapper config

--- commons/src/main/java/com/g/commons/utils/ObjectMapperUtil.java
+++ commons/src/main/java/com/g/commons/utils/ObjectMapperUtil.java
@@ -1,9 +1,20 @@
 package com.g.commons.utils;
 
+import java.text.SimpleDateFormat;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class ObjectMapperUtil {
-    private static ObjectMapper mapper = new ObjectMapper();
+    private static ObjectMapper mapper;
+
+    static {
+        mapper = new ObjectMapper();
+        mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+        mapper.setDateFormat(new SimpleDateFormat(DateTimePattern.DF_YYYY_MM_DD));
+        mapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
+    }
 
     private ObjectMapperUtil() {
     }
