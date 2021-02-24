2ac78aa9c91d9525a86748775e5f61b32d700ec6
Gaven <gaven_yeung@163.com>
2021-02-24 12:10:28 +0800

    ADD: 用户角色

--- frontend/src/pages/sys/Users/index.tsx
+++ frontend/src/pages/sys/Users/index.tsx
@@ -1,16 +1,12 @@
-import React, {useRef, useState} from 'react';
+import React from 'react';
 
-import {Button, message, Space} from "antd";
-import type {FormInstance} from "antd/lib/form/Form";
-import {PageContainer} from '@ant-design/pro-layout';
-import {ModalForm, ProFormSelect, ProFormText} from "@ant-design/pro-form";
-import ProTable, {ActionType, ProColumns} from '@ant-design/pro-table';
-import {ExclamationCircleOutlined, PlusOutlined} from '@ant-design/icons';
+import {ProColumns} from "@ant-design/pro-table";
+import {ProFormSelect, ProFormText} from "@ant-design/pro-form";
 
-import {confirm, getErrorMessage} from '@/utils/utils';
+import GenericPage from "@/pages/commons/General";
 import {getOptions} from "@/pages/sys/Roles/service";
 import type {TableListItem} from './data.d';
-import {addEntity, findEntities, removeEntities, updateEntity} from "./service";
+import {findEntities} from './service';
 
 export default (): React.ReactNode => {
   const addActionUrl: string = "/api/sys/users";
@@ -27,7 +23,6 @@ export default (): React.ReactNode => {
       title: '用户账号',
       dataIndex: 'account',
       sorter: true,
-      width: '28%',
     },
     {
       title: '用户名称',
@@ -38,6 +33,13 @@ export default (): React.ReactNode => {
       title: '邮箱',
       dataIndex: 'email',
       sorter: true,
+    },
+    {
+      title: '所属用户组',
+      dataIndex: 'roles',
+      valueType: 'select',
+      request: getOptions,
+      params: {},
     },
     {
       title: '状态',
@@ -108,177 +110,17 @@ export default (): React.ReactNode => {
 
   const initValue = {'status': 'VALID'};
 
-  const actionRef = useRef<ActionType>();
-  const formRef = useRef<FormInstance>();
-  const [selectedRowsState, setSelectedRows] = useState<any[]>([]);
-  const [modalVisible, handleModalVisible] = useState<boolean>(false);
-  const [isNew, setNew] = useState<boolean>(false);
-
-  /**
-   * 添加节点
-   * @param fields
-   */
-  const handleAdd = async (fields: any) => {
-    const hide = message.loading('正在添加');
-
-    try {
-      await addEntity(addActionUrl, {...fields});
-      hide();
-      message.success('添加成功');
-      return true;
-    } catch (error) {
-      hide();
-      message.error(getErrorMessage('添加失败，请重试！', error), 10);
-      return false;
-    }
-  };
-
-  /**
-   * 更新节点
-   * @param fields
-   */
-  const handleUpdate = async (fields: any) => {
-    const hide = message.loading('正在更新');
-
-    try {
-      await updateEntity(updateActionUrl, {...fields});
-      hide();
-      message.success('更新成功');
-      return true;
-    } catch (error) {
-      hide();
-      message.error(getErrorMessage('更新失败，请重试！', error), 10);
-      return false;
-    }
-  };
-
-  /**
-   *  删除节点
-   * @param selectedRows
-   */
-  const handleRemove = async (selectedRows: any[]) => {
-    if (!selectedRows || selectedRows.length === 0) {
-      message.success('请先选择需要删除的记录');
-      return true;
-    }
-
-    const result = await confirm({
-      title: 'Confirm',
-      icon: <ExclamationCircleOutlined/>,
-      content: '确认删除所选记录？',
-      okText: '确认',
-      okType: 'danger',
-      cancelText: '取消',
-    });
-
-    if (!result) {
-      return true;
-    }
-
-    const hide = message.loading('正在删除');
-    try {
-      await removeEntities(
-        removeActionUrl,
-        selectedRows.map((row) => row.id)
-      );
-      hide();
-      message.success('删除成功，即将刷新');
-      return true;
-    } catch (error) {
-      hide();
-      message.error(getErrorMessage('删除失败，请重试！', error), 10);
-      return false;
-    }
-  };
-
-  const onRemoveClick = async (selectedRows: any[]) => {
-    const result = await handleRemove(selectedRows);
-    if (result) {
-      setSelectedRows([]);
-      actionRef.current?.reloadAndRest?.();
-    }
-  };
-
-  const optionColumn: ProColumns[] = [
-    {
-      title: '操作',
-      dataIndex: 'option',
-      valueType: 'option',
-      align: 'center',
-      render: (_, record) => [
-        <a onClick={() => {
-          setNew(false);
-          formRef.current?.resetFields();
-          formRef.current?.setFieldsValue(record);
-          handleModalVisible(true);
-        }}>
-          编辑
-        </a>,
-        <a onClick={async () => {
-          await onRemoveClick([record]);
-        }}>
-          删除
-        </a>,
-      ],
-    },
-  ];
-
   return (
-    <PageContainer>
-      <ProTable
-        actionRef={actionRef}
-        request={(params, sorter, filter) =>
-          findEntities(findActionUrl, {...params, sorter, filter})}
-        columns={tableColumns.concat(optionColumn)}
-        bordered
-        rowKey={"id"}
-        rowSelection={{
-          onChange: (_, selectedRows) => setSelectedRows(selectedRows),
-        }}
-        toolBarRender={() => [
-          <Button type="primary" key="primary" onClick={() => {
-            setNew(true);
-            formRef.current?.resetFields();
-            if (initValue) {
-              formRef.current?.setFieldsValue(initValue);
-            }
-            handleModalVisible(true)
-          }}>
-            <PlusOutlined/> 新建
-          </Button>,
-        ]}
-        tableAlertOptionRender={({onCleanSelected}) => {
-          return (
-            <Space size={16}>
-              <a onClick={() => onRemoveClick(selectedRowsState)}>批量删除</a>
-              <a onClick={onCleanSelected}>取消选择</a>
-            </Space>
-          );
-        }}
-      />
-      <ModalForm
-        formRef={formRef}
-        name="form"
-        title={isNew ? "新建" : "修改"}
-        width={"500px"}
-        visible={modalVisible}
-        onVisibleChange={handleModalVisible}
-        onFinish={async (value) => {
-          const success = isNew ?
-            await handleAdd(value) :
-            await handleUpdate(value);
-
-          if (success) {
-            handleModalVisible(false);
-
-            if (actionRef.current) {
-              actionRef.current.reload();
-            }
-          }
-        }}
-      >
-        {fromChildren}
-      </ModalForm>
-    </PageContainer>
+    <GenericPage
+      addActionUrl={addActionUrl}
+      removeActionUrl={removeActionUrl}
+      updateActionUrl={updateActionUrl}
+      findActionUrl={findActionUrl}
+      findService={findEntities}
+      tableColumns={tableColumns}
+      fromChildren={fromChildren}
+      initValue={initValue}
+      modalWidth={'500px'}
+    />
   );
 };
--- frontend/src/pages/sys/Users/service.ts
+++ frontend/src/pages/sys/Users/service.ts
@@ -24,28 +24,3 @@ export async function findEntities(url: string, params?: any) {
     }
   );
 }
-
-export async function removeEntities(url: string, params: any[]) {
-  return request(url, {
-    method: 'DELETE',
-    data: params,
-  });
-}
-
-export async function addEntity(url: string, params: any) {
-  return request(url, {
-    method: 'POST',
-    data: {
-      ...params,
-    },
-  });
-}
-
-export async function updateEntity(url: string, params: any) {
-  return request(url, {
-    method: 'PUT',
-    data: {
-      ...params,
-    },
-  });
-}
