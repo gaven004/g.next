6008415f668d3262627df6c7d31973512aba503d
Gaven <gaven_yeung@163.com>
2021-11-14 23:03:25 +0800

    调整空白页

--- main/src/main/java/com/g/sys/sec/web/MainController.java
+++ main/src/main/java/com/g/sys/sec/web/MainController.java
@@ -1,18 +1,16 @@
 package com.g.sys.sec.web;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class MainController {
-    @RequestMapping(value = "/form-login", method = RequestMethod.GET)
-    public String login(Model model) {
+    @RequestMapping(value = "/form-login")
+    public String login() {
         return "login";
     }
 
-    @RequestMapping(value = "/blank", method = RequestMethod.GET)
+    @RequestMapping(value = "/blank")
     public String blank() {
         return "blank";
     }
