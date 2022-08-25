d3b03eb337f835003d90f40d2571c8c96867745d
Gaven <gaven_yeung@163.com>
2022-08-25 22:30:16 +0800

    ADD: 仿Spring Data REST，开发自定义REST接口

---
+++ commons/src/main/java/com/g/commons/model/PagedModelLite.java
@@ -0,0 +1,185 @@
+package com.g.commons.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
+import org.springframework.hateoas.PagedModel;
+import org.springframework.lang.Nullable;
+import org.springframework.util.Assert;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+@JsonIgnoreProperties(value = {
+        "pageable",
+        "totalPages",
+        "totalElements",
+        "first",
+        "last",
+        "size",
+        "number",
+        "sort",
+        "numberOfElements",
+        "empty"
+})
+public class PagedModelLite<T> extends PageImpl<T> implements Page<T> {
+    public static PagedModelLite<?> NO_PAGE = new PagedModelLite<>();
+
+    @JsonProperty(value = "page")
+    private final PageMetadata metadata;
+
+    public PagedModelLite() {
+        this(new ArrayList<>());
+    }
+
+    public PagedModelLite(List<T> content) {
+        this(content, Pageable.unpaged(), null == content ? 0 : content.size());
+    }
+
+    public PagedModelLite(List<T> content, Pageable pageable, long total) {
+        super(content, pageable, total);
+        this.metadata = new PageMetadata(getSize(), getNumber(), getTotalElements(), getTotalPages());
+    }
+
+    public PagedModelLite(PageImpl<T> page) {
+        this(page.getContent(), page.getPageable(), page.getTotalElements());
+    }
+
+    /**
+     * Value object for pagination metadata.
+     *
+     * @author Oliver Gierke
+     */
+    public static class PageMetadata {
+
+        @JsonProperty
+        private long size;
+        @JsonProperty
+        private long totalElements;
+        @JsonProperty
+        private long totalPages;
+        @JsonProperty
+        private long number;
+
+        protected PageMetadata() {
+
+        }
+
+        /**
+         * Creates a new {@link PagedModel.PageMetadata} from the given size, number, total elements and total pages.
+         *
+         * @param size
+         * @param number        zero-indexed, must be less than totalPages
+         * @param totalElements
+         * @param totalPages
+         */
+        public PageMetadata(long size, long number, long totalElements, long totalPages) {
+
+            Assert.isTrue(size > -1, "Size must not be negative!");
+            Assert.isTrue(number > -1, "Number must not be negative!");
+            Assert.isTrue(totalElements > -1, "Total elements must not be negative!");
+            Assert.isTrue(totalPages > -1, "Total pages must not be negative!");
+
+            this.size = size;
+            this.number = number;
+            this.totalElements = totalElements;
+            this.totalPages = totalPages;
+        }
+
+        /**
+         * Creates a new {@link PagedModel.PageMetadata} from the given size, number and total elements.
+         *
+         * @param size          the size of the page
+         * @param number        the number of the page
+         * @param totalElements the total number of elements available
+         */
+        public PageMetadata(long size, long number, long totalElements) {
+            this(size, number, totalElements, size == 0 ? 0 : (long) Math.ceil((double) totalElements / (double) size));
+        }
+
+        /**
+         * Returns the requested size of the page.
+         *
+         * @return the size a positive long.
+         */
+        public long getSize() {
+            return size;
+        }
+
+        /**
+         * Returns the total number of elements available.
+         *
+         * @return the totalElements a positive long.
+         */
+        public long getTotalElements() {
+            return totalElements;
+        }
+
+        /**
+         * Returns how many pages are available in total.
+         *
+         * @return the totalPages a positive long.
+         */
+        public long getTotalPages() {
+            return totalPages;
+        }
+
+        /**
+         * Returns the number of the current page.
+         *
+         * @return the number a positive long.
+         */
+        public long getNumber() {
+            return number;
+        }
+
+        /*
+         * (non-Javadoc)
+         * @see java.lang.Object#toString()
+         */
+        @Override
+        public String toString() {
+            return String.format("Metadata { number: %d, total pages: %d, total elements: %d, size: %d }", number, totalPages,
+                    totalElements, size);
+        }
+
+        /*
+         * (non-Javadoc)
+         * @see java.lang.Object#equals(java.lang.Object)
+         */
+        @Override
+        public boolean equals(@Nullable Object obj) {
+
+            if (this == obj) {
+                return true;
+            }
+
+            if (obj == null || !obj.getClass().equals(getClass())) {
+                return false;
+            }
+
+            PageMetadata that = (PageMetadata) obj;
+
+            return this.number == that.number && this.size == that.size && this.totalElements == that.totalElements
+                    && this.totalPages == that.totalPages;
+        }
+
+        /*
+         * (non-Javadoc)
+         * @see java.lang.Object#hashCode()
+         */
+        @Override
+        public int hashCode() {
+
+            int result = 17;
+            result += 31 * (int) (this.number ^ this.number >>> 32);
+            result += 31 * (int) (this.size ^ this.size >>> 32);
+            result += 31 * (int) (this.totalElements ^ this.totalElements >>> 32);
+            result += 31 * (int) (this.totalPages ^ this.totalPages >>> 32);
+            return result;
+        }
+    }
+}
---
+++ commons/src/main/java/com/g/commons/web/CustomSearchController.java
@@ -0,0 +1,224 @@
+package com.g.commons.web;
+
+import java.lang.reflect.Method;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.MethodParameter;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.mapping.PersistentEntity;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.repository.support.RepositoryInvoker;
+import org.springframework.data.rest.core.mapping.MethodResourceMapping;
+import org.springframework.data.rest.core.mapping.ResourceMappings;
+import org.springframework.data.rest.core.mapping.ResourceMetadata;
+import org.springframework.data.rest.core.mapping.SearchResourceMappings;
+import org.springframework.data.rest.webmvc.*;
+import org.springframework.data.rest.webmvc.support.DefaultedPageable;
+import org.springframework.data.util.ClassTypeInformation;
+import org.springframework.data.util.TypeInformation;
+import org.springframework.data.web.PagedResourcesAssembler;
+import org.springframework.hateoas.server.EntityLinks;
+import org.springframework.hateoas.server.core.AnnotationAttribute;
+import org.springframework.hateoas.server.core.MethodParameters;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.Assert;
+import org.springframework.util.ClassUtils;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.bind.annotation.*;
+
+import com.g.commons.model.PagedModelLite;
+
+@BasePathAwareController
+public class CustomSearchController {
+    private static final String SEARCH = "/search";
+    private static final String BASE_MAPPING = CustomRestController.CUSTOM_REST_ROOT_MAPPING + "/{repository}" + SEARCH;
+
+    private final ResourceMappings mappings;
+    private ResourceStatus resourceStatus;
+
+    /**
+     * Creates a new {@link CustomSearchController} using the given {@link PagedResourcesAssembler},
+     * {@link EntityLinks} and {@link ResourceMappings}.
+     *
+     * @param assembler must not be {@literal null}.
+     * @param mappings  must not be {@literal null}.
+     */
+    @Autowired
+    public CustomSearchController(PagedResourcesAssembler<Object> assembler,
+                                  ResourceMappings mappings, HttpHeadersPreparer headersPreparer) {
+
+        Assert.notNull(mappings, "ResourceMappings must not be null!");
+
+        this.mappings = mappings;
+        this.resourceStatus = ResourceStatus.of(headersPreparer);
+    }
+
+    /**
+     * Executes the search with the given name.
+     *
+     * @param resourceInformation
+     * @param parameters
+     * @param search
+     * @param pageable
+     * @param sort
+     * @param assembler
+     * @return
+     * @throws ResourceNotFoundException
+     */
+    @ResponseBody
+    @RequestMapping(value = BASE_MAPPING + "/{search}", method = RequestMethod.GET)
+    public ResponseEntity<?> executeSearch(RootResourceInformation resourceInformation,
+                                           @RequestParam MultiValueMap<String, Object> parameters,
+                                           @PathVariable String search,
+                                           DefaultedPageable pageable,
+                                           Sort sort,
+                                           PersistentEntityResourceAssembler assembler,
+                                           @RequestHeader HttpHeaders headers) {
+
+        Method method = checkExecutability(resourceInformation, search);
+        Optional<Object> result = executeQueryMethod(resourceInformation.getInvoker(), parameters, method, pageable, sort,
+                assembler);
+
+        SearchResourceMappings searchMappings = resourceInformation.getSearchMappings();
+        MethodResourceMapping methodMapping = searchMappings.getExportedMethodMappingForPath(search);
+        Class<?> domainType = methodMapping.getReturnedDomainType();
+
+        return toModel(result, assembler, domainType, headers, resourceInformation);
+    }
+
+    /**
+     * @param source     can be must not be {@literal null}.
+     * @param assembler  must not be {@literal null}.
+     * @param domainType the domain type in case the source is an empty iterable, must not be {@literal null}.
+     * @return
+     */
+    protected ResponseEntity<?> toModel(Optional<Object> source,
+                                        final PersistentEntityResourceAssembler assembler,
+                                        Class<?> domainType,
+                                        HttpHeaders headers,
+                                        RootResourceInformation information) {
+
+        return source.map(it -> {
+
+            if (it instanceof PageImpl<?>) {
+                return ResponseEntity.ok(new PagedModelLite<>((PageImpl) it));
+            } else if (it instanceof Iterable) {
+                return ResponseEntity.ok(it);
+            } else if (ClassUtils.isPrimitiveOrWrapper(it.getClass())) {
+                return ResponseEntity.ok(it);
+            }
+
+            PersistentEntity<?, ?> entity = information.getPersistentEntity();
+
+            // Returned value is not of the aggregates type - probably some projection
+            if (!entity.getType().isInstance(it)) {
+                return ResponseEntity.ok(it);
+            }
+
+            return resourceStatus.getStatusAndHeaders(headers, it, entity).toResponseEntity(it);
+
+        }).orElseThrow(() -> new ResourceNotFoundException());
+    }
+
+    /**
+     * Checks that the given request is actually executable. Will reject execution if we don't find a search with the
+     * given name.
+     *
+     * @param resourceInformation
+     * @param searchName
+     * @return
+     */
+    private Method checkExecutability(RootResourceInformation resourceInformation, String searchName) {
+
+        SearchResourceMappings searchMapping = verifySearchesExposed(resourceInformation);
+
+        Method method = searchMapping.getMappedMethod(searchName);
+
+        if (method == null) {
+            throw new ResourceNotFoundException();
+        }
+
+        return method;
+    }
+
+    private Optional<Object> executeQueryMethod(final RepositoryInvoker invoker,
+                                                @RequestParam MultiValueMap<String, Object> parameters,
+                                                Method method, DefaultedPageable pageable, Sort sort,
+                                                PersistentEntityResourceAssembler assembler) {
+
+        MultiValueMap<String, Object> result = new LinkedMultiValueMap<String, Object>(parameters);
+        MethodParameters methodParameters = new MethodParameters(method, new AnnotationAttribute(Param.class));
+        List<MethodParameter> parameterList = methodParameters.getParameters();
+        List<TypeInformation<?>> parameterTypeInformations = ClassTypeInformation.from(method.getDeclaringClass())
+                .getParameterTypes(method);
+
+        parameters.entrySet().forEach(entry ->
+
+                methodParameters.getParameter(entry.getKey()).ifPresent(parameter -> {
+
+                    int parameterIndex = parameterList.indexOf(parameter);
+                    TypeInformation<?> domainType = parameterTypeInformations.get(parameterIndex).getActualType();
+
+                    ResourceMetadata metadata = mappings.getMetadataFor(domainType.getType());
+
+                    if (metadata != null && metadata.isExported()) {
+                        result.put(parameter.getParameterName(), prepareUris(entry.getValue()));
+                    }
+                }));
+
+        return invoker.invokeQueryMethod(method, result, pageable.getPageable(), sort);
+    }
+
+
+    /**
+     * Verifies that the given {@link RootResourceInformation} has searches exposed.
+     *
+     * @param resourceInformation
+     */
+    private static SearchResourceMappings verifySearchesExposed(RootResourceInformation resourceInformation) {
+
+        SearchResourceMappings resourceMappings = resourceInformation.getSearchMappings();
+
+        if (!resourceMappings.isExported()) {
+            throw new ResourceNotFoundException();
+        }
+
+        return resourceMappings;
+    }
+
+    /**
+     * Tries to turn all elements of the given {@link List} into URIs and falls back to keeping the original element if
+     * the conversion fails.
+     *
+     * @param source can be {@literal null}.
+     * @return
+     */
+    private static List<Object> prepareUris(List<Object> source) {
+
+        if (source == null || source.isEmpty()) {
+            return Collections.emptyList();
+        }
+
+        List<Object> result = new ArrayList<Object>(source.size());
+
+        for (Object element : source) {
+
+            try {
+                result.add(new URI(element.toString()));
+            } catch (URISyntaxException o_O) {
+                result.add(element);
+            }
+        }
+
+        return result;
+    }
+}
